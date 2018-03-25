99d405eceb943714f3bf17ac2ee2fae03e96c6ee
mahadev <mahadev>
2018-03-25 14:39:11 +0530

    Anna

--- app/src/main/java/com/example/ab/myapplication/Book.java
+++ app/src/main/java/com/example/ab/myapplication/Book.java
@@ -1,5 +1,7 @@
 package com.example.ab.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -17,7 +19,7 @@ import org.json.JSONObject;
 
 import java.util.Date;
 
-public class BookASlotActivity extends AppCompatActivity implements View.OnClickListener {
+public class Book extends AppCompatActivity implements View.OnClickListener {
 
     TimePicker startTimePicker, endTimePicker;
     Button confirmBooking;
@@ -26,7 +28,7 @@ public class BookASlotActivity extends AppCompatActivity implements View.OnClick
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_booking);
+        setContentView(R.layout.activity_book);
         startTimePicker = (TimePicker) findViewById(R.id.tpStart);
         endTimePicker = (TimePicker) findViewById(R.id.tpEnd);
         confirmBooking = (Button) findViewById(R.id.bt_confirm_booking);
@@ -42,19 +44,30 @@ public class BookASlotActivity extends AppCompatActivity implements View.OnClick
         Toast.makeText(this, "Booking your slot..", Toast.LENGTH_SHORT).show();
         int start = startTimePicker.getHour();
         int end = startTimePicker.getMinute();
-        try {
-            new BookingTask(selectedSlotId,start,end, this, new Response.Listener() {
-                @Override
-                public void onResponse(Object response) {
-                    try {
-                        JSONObject loginResponse = new JSONObject(response.toString());
-                    } catch (JSONException e) {
-                        System.out.println("Failed to parse response object");
-                    }
-                }
-            }).execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            new BookingTask(selectedSlotId,start,end, this, new Response.Listener() {
+//                @Override
+//                public void onResponse(Object response) {
+//                    try {
+//                        JSONObject loginResponse = new JSONObject(response.toString());
+//                    } catch (JSONException e) {
+//                        System.out.println("Failed to parse response object");
+//                    }
+//                }
+//            }).execute();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        String dlattitude = "18.520430";
+        String dlongitude = "73.856744";
+
+        String slattitude = "17.902781";
+        String slongitude = "74.081415";
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?" + "saddr=" + slattitude + "," + slongitude + "&daddr=" + dlattitude + "," + dlongitude));
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+        startActivity(intent);
+
     }
 }
